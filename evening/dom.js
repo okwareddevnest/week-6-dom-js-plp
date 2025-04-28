3b10be034ba6d81e223970ee9ffff5b701633cfe
okwareddevnest <softengdedan@gmail.com>
2025-04-28 20:20:06 +0300

    evening classes

---
+++ evening/dom.js
@@ -0,0 +1,13 @@
+//By ID
+const elementID = document.getElementById("myElement");
+
+// By Class Name
+const elementCls = document.getElementsByClassName("myClass");
+
+// By Tag Name
+const paragraphs = document.getElementsByTagName("p");
+
+// Query Selectors
+const element = document.querySelector("#myElement");
+
+const elements = document.querySelectorAll(".myClass");
